d60e59c2058af76f9921d03ed74c62dd1544c967
krisbratvold <kris.bratvold@gmail.com>
2021-07-20 13:29:22 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link, Redirect, Router} from "@reach/router"
+
+import NotFound from './views/NotFound';
+import Form from './views/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Form path="/product/new"></Form>
+        <Redirect from="/" to="/product/new" noThrow="true"></Redirect>
+        <NotFound default></NotFound>
+      </Router>
     </div>
   );
 }
---
+++ src/views/Form.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import {navigate} from "@reach/router"
+import axios from "axios";
+
+
+const Form = (props) => {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [description, setDescrition] = useState("");
+
+    const createProduct = e => {
+        e.preventDefault();
+        const newProduct = {
+            title : title,
+            price : price,
+            description : description
+        };
+        axios.post("http://localhost:5000/api/products/new", newProduct)
+        .then((res)=>{
+            console.log(res);
+            navigate("/")
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    return <div>
+        <h1>Product Manager</h1>
+        <form onSubmit={(e) => {
+            createProduct(e);
+        }}>
+            <label>Title:</label>
+            <input type="text" onChange={(e) => {setTitle(e.target.value)}} />
+            <label>Price:</label>
+            <input type="number" onChange={(e) => {setPrice(e.target.value)}} />
+            <label>Description:</label>
+            <input type="text" onChange={(e) => {setDescrition(e.target.value)}} />
+            <button>Submit</button>
+        </form>
+        </div>
+}
+
+export default Form;
